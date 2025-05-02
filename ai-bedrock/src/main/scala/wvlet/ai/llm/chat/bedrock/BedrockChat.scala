2fba9471babec9500376e83d2865954c71033f74
Taro L. Saito <leo@xerial.org>
2025-05-01 21:19:29 -0700

    feature: Add chat model interfaces (#2)

---
+++ ai-bedrock/src/main/scala/wvlet/ai/llm/chat/bedrock/BedrockChat.scala
@@ -0,0 +1,6 @@
+package wvlet.ai.llm.chat.bedrock
+
+import wvlet.ai.llm.chat.{ChatModel, ChatRequest}
+
+class BedrockChat extends ChatModel:
+  override def chat(request: ChatRequest): Unit = ???
---
+++ ai-core/src/main/scala/wvlet/ai/llm/chat/ToolSpec.scala
@@ -0,0 +1,17 @@
+package wvlet.ai.llm.chat
+
+case class ToolSpec(
+    name: String,
+    description: String,
+    parameters: List[ToolParameter],
+    returnType: ToolReturnType
+)
+
+case class ToolReturnType(dataType: String, description: String)
+case class ToolParameter(
+    name: String,
+    description: String,
+    dataType: String,
+    required: Boolean,
+    defaultValue: Option[Any] = None
+)
