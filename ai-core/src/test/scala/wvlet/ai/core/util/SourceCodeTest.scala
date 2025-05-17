8c736cc78d86d319299f0f2fd47dfe67a81c4f57
Taro L. Saito <leo@xerial.org>
2025-05-17 10:32:02 -0700

    feature: Update line number in SourceCodeTest

--- ai-core/src/test/scala/wvlet/ai/core/util/SourceCodeTest.scala
+++ ai-core/src/test/scala/wvlet/ai/core/util/SourceCodeTest.scala
@@ -7,6 +7,6 @@ class SourceCodeTest extends AirSpec:
   test("embed the source code name and location") {
     val sc = SourceCode()
     sc.fileName shouldBe "SourceCodeTest.scala"
-    sc.line shouldBe 7
+    sc.line shouldBe 8
     sc.col shouldBe 25
   }
