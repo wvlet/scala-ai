429e872ce58de3f5dd492acdb3a82b07931e9642
Taro L. Saito <leo@xerial.org>
2025-05-18 21:06:29 -0700

    feature: Add wvlet.ai.core.control module

---
+++ ai-core/src/main/scala/wvlet/ai/core/control/ResultClass.scala
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wvlet.ai.core.control
+
+import wvlet.ai.core.control.Retry.ExtraWait
+
+/**
+  * A classification of the code execution result
+  */
+sealed trait ResultClass
+
+object ResultClass:
+
+  /**
+    * A label for the successful code execution result
+    */
+  case object Succeeded extends ResultClass
+
+  /**
+    * A label for the failed code execution result
+    */
+  case class Failed(
+      isRetryable: Boolean,
+      cause: Throwable,
+      extraWait: ExtraWait = Retry.noExtraWait
+  ) extends ResultClass:
+    def withExtraWaitMillis(extraWaitMillis: Int): Failed = this.copy(extraWait =
+      ExtraWait(maxExtraWaitMillis = extraWaitMillis)
+    )
+
+    def withExtraWaitFactor(factor: Double, maxExtraWaitMillis: Int = 5000): Failed = this.copy(
+      extraWait = ExtraWait(maxExtraWaitMillis = maxExtraWaitMillis, factor = factor)
+    )
+
+  def retryableFailure(e: Throwable): Failed    = Retry.retryableFailure(e)
+  def nonRetryableFailure(e: Throwable): Failed = Retry.nonRetryableFailure(e)
+
+  def ALWAYS_SUCCEED: Any => ResultClass = (x: Any) => Succeeded
+
+  def ALWAYS_RETRY: Throwable => ResultClass.Failed = (e: Throwable) => retryableFailure(e)
---
+++ ai-core/src/test/scala/wvlet/ai/core/control/RetryTest.scala
@@ -0,0 +1,162 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wvlet.ai.core.control
+
+import wvlet.ai.core.control.Retry.{MaxRetryException, RetryContext}
+import wvlet.airspec.AirSpec
+
+import scala.concurrent.TimeoutException
+
+/**
+  */
+class RetryTest extends AirSpec:
+
+  test("support backoff retry") {
+    var count = 0
+
+    val r = Retry.withBackOff(maxRetry = 3)
+
+    val e = r.run {
+      count += 1
+      logger.info("hello retry")
+      if count < 3 then
+        throw new IllegalStateException("retry test")
+      else
+        "success"
+    }
+
+    e shouldBe "success"
+    count shouldBe 3
+  }
+
+  test("support bounded backoff retry") {
+    val maxWait = 50000
+    var r =
+      Retry
+        .withBoundedBackoff(initialIntervalMillis = 1000, maxTotalWaitMillis = maxWait)
+        .noRetryLogging
+
+    r = r.init()
+    var waitTotal = 0
+    while r.canContinue do
+      waitTotal += r.nextWaitMillis
+      r = r.nextRetry(new IllegalStateException())
+    waitTotal <= maxWait shouldBe true
+  }
+
+  test("support jitter retry") {
+    var count = 0
+
+    val r = Retry
+      .withJitter(maxRetry = 3)
+      .run {
+        count += 1
+        logger.info("hello retry")
+        if count < 2 then
+          throw new IllegalStateException("retry test")
+        else
+          "success"
+      }
+
+    r shouldBe "success"
+    count shouldBe 2
+  }
+
+  test("throw max retry exception") {
+    val e = intercept[MaxRetryException] {
+      Retry
+        .withBackOff(maxRetry = 3)
+        .retryOn { case e: IllegalStateException =>
+          warn(e.getMessage)
+          ResultClass.retryableFailure(e)
+        }
+        .run {
+          logger.info("hello retry")
+          throw new IllegalStateException("retry test")
+        }
+    }
+
+    e.retryContext.maxRetry shouldBe 3
+    e.retryContext.retryCount shouldBe 3
+    e.retryContext.lastError.getClass shouldBe classOf[IllegalStateException]
+  }
+
+  test("change retry wait strategy") {
+    val r = Retry.withJitter().withBackOff(initialIntervalMillis = 3)
+
+    r.retryWaitStrategy.retryPolicyConfig.initialIntervalMillis shouldBe 3
+
+    val j = r.withJitter(initialIntervalMillis = 20)
+    j.retryWaitStrategy.retryPolicyConfig.initialIntervalMillis shouldBe 20
+
+    val m = j.withMaxRetry(100)
+    m.maxRetry shouldBe 100
+  }
+
+  test("pass the execution context") {
+    val r = Retry.withBackOff(initialIntervalMillis = 0)
+
+    var count   = 0
+    var checked = false
+    r.beforeRetry { (ctx: RetryContext) =>
+        ctx.context shouldBe Some("hello world")
+        checked = true
+      }
+      .runWithContext("hello world") {
+        if count == 0 then
+          count += 1
+          throw new Exception()
+      }
+
+    count shouldBe 1
+    checked shouldBe true
+  }
+
+  test("add extra wait") {
+    intercept[MaxRetryException] {
+      Retry
+        .withBackOff(initialIntervalMillis = 10)
+        .withMaxRetry(1)
+        .withErrorClassifier { case e: TimeoutException =>
+          ResultClass.retryableFailure(e).withExtraWaitMillis(100)
+        }
+        .beforeRetry { (ctx: RetryContext) =>
+          debug(s"${ctx.retryCount} ${ctx.nextWaitMillis}")
+          ctx.nextWaitMillis shouldBe 10 + 100
+        }
+        .run {
+          throw new TimeoutException()
+        }
+    }
+  }
+
+  test("add extra wait factor") {
+    intercept[MaxRetryException] {
+      Retry
+        .withBackOff(initialIntervalMillis = 10)
+        .withMaxRetry(1)
+        .withErrorClassifier { case e: TimeoutException =>
+          ResultClass.retryableFailure(e).withExtraWaitFactor(0.2)
+        }
+        .beforeRetry { (ctx: RetryContext) =>
+          debug(s"${ctx.retryCount} ${ctx.nextWaitMillis}")
+          ctx.nextWaitMillis shouldBe 10 + 2
+        }
+        .run {
+          throw new TimeoutException()
+        }
+    }
+  }
+
+end RetryTest
